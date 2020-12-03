3be0f03c8b6433e53bef7990635c37332699ac22
adrian154 <94568a@gmail.com>
2020-12-03 09:06:21 -0800

    fixes

--- Day2/Part1/solution.js
+++ Day2/Part1/solution.js
@@ -1,8 +1,3 @@
 // same running syntax as day 1
 // epic one liner!!
-console.log(String(require("fs").readFileSync("../data.txt")).split("\n").map(str => ({
-    min: Number(str.substr(0, str.indexOf("-"))),
-    max: Number(str.match(/-\d*/)[0].substr(1)),
-    char: str[str.indexOf(":") - 1],
-    pw: str.substr(str.indexOf(":") + 2, str.length)
-})).reduce((accum, cur) => accum + Number((cur.pw.split(cur.char).length - 1 <= cur.max && cur.pw.split(cur.char).length - 1 >= cur.min)), 0));
+console.log(String(require("fs").readFileSync("../data.txt")).split("\n").map(str => ({min: Number(str.substr(0, str.indexOf("-"))), max: Number(str.match(/-\d*/)[0].substr(1)), char: str[str.indexOf(":") - 1], pw: str.substr(str.indexOf(":") + 2, str.length)})).reduce((accum, cur) => accum + Number((cur.pw.split(cur.char).length - 1 <= cur.max && cur.pw.split(cur.char).length - 1 >= cur.min)), 0));
