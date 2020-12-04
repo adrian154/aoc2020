1adc33bf8ce6eaf7da0ca5ad595e31a7057deba4
adrian154 <94568a@gmail.com>
2020-12-04 08:26:32 -0800

    start day 4

--- Day3/Part1/solution.js
+++ Day3/Part1/solution.js
@@ -1,22 +1,3 @@
-const data = String(require("fs").readFileSync("../data.txt")).split("\n");
-
-const trees = (dx, dy) => {
-
-    let x = 0;
-    let numTrees = 0;
-    for(let y = 0; y < data.length; y += dy) {
-        
-        numTrees += data[y][x % (data[0].length - 1)] == '#';
-        x += dx;
-
-    }
-
-    return numTrees;
-
-};
+const { trees } = require("../trees.js");
 
 console.log(trees(3, 1));
-
-module.exports = {
-    trees: trees
-};
--- Day3/Part2/solution.js
+++ Day3/Part2/solution.js
@@ -1,3 +1,3 @@
-const { trees } = require("../Part1/solution.js");
+const { trees } = require("../trees.js");
 
 console.log(trees(1, 1) * trees(3, 1) * trees(5, 1) * trees(7, 1) * trees(1, 2));
